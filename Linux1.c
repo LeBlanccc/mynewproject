85120de8d2a5579e59d5d6db7847d661a811c427
LeBlanccc <864585373@qq.com>
2015-01-05 16:55:40 +0800

    Delete Linux1.c

--- Linux1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-main ()
-{
-    char a;
-    printf("input one letter from the keyboard:");
-    scanf("%c",&a);
-    printf("%d\n",a); //输入一个字符，然后输出这个字符的                          ASICII码
-}
